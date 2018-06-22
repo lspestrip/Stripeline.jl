07357f8dfa5b352a53e37af48e7168abfe8bd98f
Maurizio Tomasi <maurizio.tomasi@unimi.it>
2018-06-22 10:53:55 +0200

    Add more tests to InstrumentDB

--- test/instrumentdb_tests.jl
+++ test/instrumentdb_tests.jl
@@ -2,3 +2,11 @@ db = InstrumentDB(joinpath(Pkg.dir("Stripeline"), "instrumentdb"))
 
 @test length(db.focalplane) ≥ 49
 @test length(db.detectors) ≥ 55
+@test "I0" in keys(db.focalplane)
+
+# Now test that everything works without specifying the paths
+
+defaultdb = InstrumentDB()
+
+@test sort(collect(keys(defaultdb.focalplane))) == sort(collect(keys(db.focalplane)))
+@test sort(collect(keys(defaultdb.detectors))) == sort(collect(keys(db.detectors)))
