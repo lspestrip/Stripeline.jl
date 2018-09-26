7caaad200e4b912fde08586faee9416b30b4336c
Silvia Caprioli <silvia.caprioli@unimi.it>
2018-09-26 12:21:04 +0200

    fixed wrong test for get_chunk_properties

--- test/tod_splitter_tests.jl
+++ test/tod_splitter_tests.jl
@@ -17,7 +17,7 @@ baselines_per_process = [6, 7, 7]
 
 chunks =  [[datachunk(1, 1, 5, 5), datachunk(2, 1, 1, 1)], [datachunk(2, 2, 5, 4), datachunk(3, 1, 3, 3)], [datachunk(3, 4, 5, 2), datachunk(4, 1, 5, 5)]]
 
-@test get_chunk_properties(chunks, baseline_length_s, fsamp_hz, rank) ==  ([2,3], [10.0, 0.0], [50.0, 30.0], [4, 3], [400, 300])
+@test get_chunk_properties(chunks, baseline_length_s, fsamp_hz, rank) ==  ([2,3], [10.0, 0.0], [49.901, 29.901], [4, 3], [400, 300])
 
 chunks2 = [[datachunk(1, 1, 1000, 1000), datachunk(2, 1, 1000, 1000)]]
 baselines_per_process2 = 2000
