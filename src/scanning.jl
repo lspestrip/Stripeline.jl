28adb5b138453ddcda91729aa2fb7e3eee3508be
Maurizio Tomasi <ziotom78@gmail.com>
2019-03-01 15:40:13 +0100

    Add 'polaxis' keyword to 'generatepointings'

--- src/scanning.jl
+++ src/scanning.jl
@@ -1,3 +1,77 @@
+# -*- encoding: utf-8 -*-
+#
+# MIT License
+#
+# Copyright (c) 2016 Maurizio Tomasi
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+#
+# # TERMINOLOGY
+#
+# The code in this file tries to follow the conventions laid in
+# document LSPE-STRIP-SP-017 ("STRIP Telescope Coordinate Systems
+# Specification") as much as possible. As the reader might not be
+# aquainted with it, here is a short summary of the relevant
+# terminology:
+#
+# +-----+-----------------------------+------------------------------------------+
+# | SCS | Site Coordinate System      | Centered on the GPS station              |
+# +-----+-----------------------------+------------------------------------------+
+# | MCS | Mount Coordinate System     | Centered at the basis of the telescope   |
+# +-------+---------------------------+------------------------------------------+
+# | ACS | A zimuth Coordinate System  | Moveable, centered in the middle of      |
+# |     |                             | ground motor. Positive angles go from    |
+# |     |                             | North to *East* (beware!)                |
+# +-----+-----------------------------+------------------------------------------+
+# | ECS | Elevation Coordinate System | Moveable, the Y axis is aligned with     |
+# |     |                             | the motor axis                           |
+# +-----+-----------------------------+------------------------------------------+
+# | BCS | Boresight Coordinate System | It should have been moveable, but the    |
+# |     |                             | current design does not allow it (alas!) |
+# +-----+-----------------------------+------------------------------------------+
+# | RDP | Reference Detector Plane    | Centered in the focus of the telescope.  |
+# |     |                             | All the beam vectors are specified in    |
+# |     |                             | this frame.                              |
+# +-----+-----------------------------+------------------------------------------+
+#
+# We always use a right-handed coordinate system:
+#
+#                 ^ Z axis (aligned with "up" direction, e.g., Zenith)
+#                 |
+#                 |
+#                 |
+#                 |
+#                 |
+#                 |
+#                 |
+#                 |
+#                 |
+#          Origin O----------------> Y axis
+#                /
+#               /
+#              /
+#             /
+#            /
+#           v  X axis (aligned with North, when applicable)
+#
+
 import Quaternions
 import Healpix
 import StaticArrays
@@ -88,11 +162,11 @@ end
 """
 function telescopetoground(wheelanglesfn, time_s)
     (wheel1ang, wheel2ang, wheel3ang) = wheelanglesfn(time_s)
-    
+
     qwheel1 = qrotation_z(wheel1ang)
     qwheel2 = qrotation_y(wheel2ang)
     qwheel3 = qrotation_z(-wheel3ang)
-    
+
     qwheel3 * (qwheel2 * qwheel1)
 end
 
@@ -113,7 +187,7 @@ The keyword `day_duration_s` specifies the length of a day in seconds.
 function groundtoearth(groundq, time_s, latitude_deg; day_duration_s=86400.0)
     locq = qrotation_y(deg2rad(90 - latitude_deg))
     earthq = qrotation_z(2π * time_s / day_duration_s)
-    
+
     earthq * (locq * groundq)
 end
 
@@ -121,18 +195,18 @@ end
 """
     vector2equatorial(dir, jd, latitude_deg, longitude_deg, height_m)
 
-Transform the Healpix coordinates of a vector into equatorial coordinates. 
-The parameter `vector` is 3D vector, `jd` is the julian date. The 
-paramters `latitude_deg`, `longitude_deg` and `height_m` should contain the 
+Transform the Healpix coordinates of a vector into equatorial coordinates.
+The parameter `vector` is 3D vector, `jd` is the julian date. The
+paramters `latitude_deg`, `longitude_deg` and `height_m` should contain the
 latitude (in degrees, N is positive), the longitude (in degrees, counterclockwise
-is positive) and the height (in meters) of the location where the observation is 
-made. 
+is positive) and the height (in meters) of the location where the observation is
+made.
 """
 function vector2equatorial(vector, jd, latitude_deg, longitude_deg, height_m)
     (θ, ϕ) = Healpix.vec2ang(vector...)
-    alt_rad = π/2 - θ 
+    alt_rad = π/2 - θ
     az_rad = 2π - ϕ
-    
+
     ra_deg, dec_deg, _ = AstroLib.hor2eq(rad2deg(alt_rad),
                                          rad2deg(az_rad),
                                          jd,
@@ -158,7 +232,7 @@ function polarizationangle(northdir, poldir)
     cosψ = clamp(dot(northdir, poldir), -1, 1)
     crosspr = northdir × poldir
     sinψ = clamp(sqrt(dot(crosspr, crosspr)), -1, 1)
-    ψ = atan(sinψ, cosψ) 
+    ψ = atan(sinψ, cosψ)
     ψ
 end
 
@@ -186,7 +260,7 @@ function quat_to_angles(boreaxis, polaxis, quat)
     rotmatr = Quaternions.rotationmatrix(quat)
     boresight = rotmatr * boreaxis
     poldir = rotmatr * polaxis
-            
+
     (θ, ϕ) = Healpix.vec2ang(boresight...)
 
     northdir = get_northdir(θ, ϕ)
@@ -197,6 +271,7 @@ end
 function genpointings(wheelanglesfn,
                       dir,
                       timerange_s;
+                      polaxis=Float64[1.0, 0.0, 0.0],
                       latitude_deg=0.0,
                       ground=false)
 
@@ -208,22 +283,21 @@ function genpointings(wheelanglesfn,
         ψ = Array{Float64}(undef, length(timerange_s))
     end
 
-    zaxis = [1; 0; 0] #should be different for each horn...
     for (idx, time_s) = enumerate(timerange_s)
-        
+
         # This converts the RDP into the MCS (ground reference frame)
         groundq = telescopetoground(wheelanglesfn, time_s)
         # This converts the MCS into the celestial reference frame
         quat = groundtoearth(groundq, time_s, latitude_deg)
 
-        θ, ϕ, curψ = quat_to_angles(dir, zaxis, quat)
+        θ, ϕ, curψ = quat_to_angles(dir, polaxis, quat)
 
-        (dirs[idx, 1], dirs[idx, 2]) = (θ, ϕ) 
+        (dirs[idx, 1], dirs[idx, 2]) = (θ, ϕ)
         northdir = get_northdir(θ, ϕ)
-        
+
         if ground
             # Re-run the transformation algorithm using the ground quaternion
-            θ_ground, ϕ_ground, ψ_ground = quat_to_angles(dir, zaxis, groundq)
+            θ_ground, ϕ_ground, ψ_ground = quat_to_angles(dir, polaxis, groundq)
 
             (dirs[idx, 1], dirs[idx, 2], dirs[idx, 3], dirs[idx, 4]) = (θ, ϕ, θ_ground, ϕ_ground)
             (ψ[idx, 1], ψ[idx, 2]) = (curψ, ψ_ground)
@@ -232,7 +306,7 @@ function genpointings(wheelanglesfn,
             ψ[idx] = curψ
         end
     end
-    
+
     (dirs, ψ)
 end
 
@@ -241,10 +315,11 @@ function genpointings(wheelanglesfn,
                       dir,
                       timerange_s,
                       t_start;
+                      polaxis=Float64[1.0, 0.0, 0.0],
                       latitude_deg=0.0,
                       longitude_deg=0.0,
                       height_m=0)
-    
+
     skydirs = Array{Float64}(undef, length(timerange_s), 2)
     skyψ = Array{Float64}(undef, length(timerange_s))
 
@@ -253,7 +328,7 @@ function genpointings(wheelanglesfn,
 
         # This is in the ground reference frame
         groundq = telescopetoground(wheelanglesfn, time_s)
-        
+
         rotmatr = Quaternions.rotationmatrix(groundq)
         vector = rotmatr * dir
 
@@ -273,14 +348,14 @@ function genpointings(wheelanglesfn,
         #                                           latitude_deg,
         #                                           longitude_deg,
         #                                           height_m)
-        
+
         # skypoldir = StaticArrays.@SArray [cos(Decpol_rad) * cos(Rapol_rad),
         #                                   cos(Decpol_rad) * sin(Rapol_rad),
         #                                   sin(Decpol_rad)]
         # skynorthdir = StaticArrays.@SArray [-sin(Decpol_rad) * cos(Rapol_rad),
         #                                     -sin(Decpol_rad) * sin(Rapol_rad),
         #                                     cos(Decpol_rad)]
-        
+
         # skyψ[idx] = polarizationangle(skynorthdir, skypoldir)
         skyψ[idx] = 0.
     end
@@ -290,9 +365,11 @@ end
 
 
 @doc raw"""
-    genpointings(wheelanglesfn, dir, timerange_s; latitude_deg=0.0, 
-                 ground=false)
-    genpointings(wheelanglesfn, dir, timerange_s, t_start, t_stop; 
+    genpointings(wheelanglesfn, dir, timerange_s; 
+                 polaxis=Float64[1.0, 0.0, 0.0],
+                 latitude_deg=0.0, ground=false)
+    genpointings(wheelanglesfn, dir, timerange_s, t_start, t_stop;
+                 polaxis=Float64[1.0, 0.0, 0.0],
                  latitude_deg=0.0, longitude_deg=0.0, height_m=0.0)
 
 Generate a set of pointings for a STRIP detector. The parameter
@@ -309,7 +386,7 @@ the three motors:
 3. The ground motor (rotation around the ``z`` axis, **clockwise**:
    N→E→S→W)
 
-The meaning of the parameters is the following:
+The meaning of the parameters/keywords is the following:
 
 - `dir` specifies the pointing direction of the mean (the boresight is
   [0, 0, 1]). It must be normalized.
@@ -325,6 +402,7 @@ is made (in degrees, North is positive).
   coordinates (columns 1 and 2) and in ground coordinates (columns 3
   and 4). If false, only the Equatorial coordinates are computed.
 
+- `polaxis` is the polarization axis; it must be normalized
 
 # Return values
 
@@ -359,9 +437,9 @@ notation.
 import Dates
 
 dirs, psi = genpointings(time_s -> (0, deg2rad(20), timetorotang(time_s, 1)),
-                         [0, 0, 1], 
-                         0:0.1:1, 
-                         Dates.DateTime(2019, 01, 01, 0, 0, 0), 
+                         [0, 0, 1],
+                         0:0.1:1,
+                         Dates.DateTime(2019, 01, 01, 0, 0, 0),
                          latitude_deg=10.0,
                          longitude_deg=20.0,
                          height_m = 1000) do time_s
