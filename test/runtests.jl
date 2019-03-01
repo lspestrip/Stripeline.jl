8022c3d400ce899605f28d46ee8ad303d337a101
Maurizio Tomasi <ziotom78@gmail.com>
2019-03-01 18:27:28 +0100

    Create testsets

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,38 +1,22 @@
 using Test
 using Stripeline
 
-# Scanning strategy
-
-(dirs, psi) = genpointings([0., 0., 1.], 0.:20.:60., latitude_deg=0.0) do time_s
-    wheel1ang = 0.0
-    wheel2ang = 30.0
-    wheel3ang = timetorotang(time_s, 1.0)
-
-    (wheel1ang, wheel2ang, wheel3ang)
+@testset "Instrument database" begin
+    include("instrumentdb_tests.jl")
+end
+    
+@testset "Scanning strategy" begin
+    include("scanning.jl")
 end
 
-@test size(dirs) == (4, 2)
-@test length(psi) == 4
-#@test dirs ≈ [0.15487 4.71239; 2.0875 3.3214; 2.0875 6.10774; 0.15487 4.71675], atol = 1e-5
-# This fails, but we have to revise the way polarization angles are calculated
-#@test psi ≈ [0, π / 2, π, 3π / 4]
+@testset "Map maker" begin
+    include("map_maker_tests.jl")
+end
 
-# Instrument DB
+@testset "TOD splitter" begin
+    include("tod_splitter_tests.jl")
+end
 
-include("instrumentdb_tests.jl")
-
-# Map-maker
-
-include("map_maker_tests.jl")
-
-# TOD-splitter
-
-include("tod_splitter_tests.jl")
-
-# Noise Generation
-
-include("noisegeneration_tests.jl")
-
-# Scanning 
-
-include("scanning.jl")
+@testset "Noise generation" begin
+    include("noisegeneration_tests.jl")
+end
